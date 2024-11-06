88d35f24f106c47386de830e575d839ac4961822
Lauren Harrison <laurenharrison@Laurens-MacBook-Pro.local>
2024-11-06 16:23:51 +0000

    FINAL VERSION with comments tidied up

--- restock.py
+++ restock.py
@@ -1,26 +1,12 @@
 def restock_inventory(available_items, sold_units, inventory_records, current_day):
-    '''
-***********COMPLETE THIS FUNCTION***********
-This function is responsible for updating the stock/restock for a given day.
----------------
-Function Input:
----------------
-available_items: (integer) Available Tshirts from the previous day.
-inventory_records: (List) A list of inventory records until the previous day. Each row contains (day, sales, restocked items, available items)
-current_day: (integer) Day number which you want to add as the current day. 
-----------------
-Function Output:
-----------------
-available_items:(integer) This function returns this integer which updates the available items at the current day.
 
-
-The function will also update the inventory_records (For restocking) for a  given current day. It will also return "available_items".
-    '''
-## the first stocking begins on day 0
-    #occurs every 7 days
-## initial stock level is set to 2000 units (and may not exceed this value)
 ## MY COMMENTS
     # don't need to do for loop for 0-50 as the function itself is being called 49 times in simulation
+    ## the first stocking begins on day 0
+    #occurs every 7 days
+    ## initial stock level is set to 2000 units (and may not exceed this value)
+
+    #checking whether restock is necessary
     if current_day == 0:
         restock_needed = True
 
@@ -28,52 +14,29 @@ The function will also update the inventory_records (For restocking) for a  give
         restock_needed = True
     else:
         restock_needed = False
-    ## if statements rather than while?
+    #if true, work out difference between 2000 (max stock level) and items available to know how much stock is to be replenished
     if restock_needed:
         restocked_units = 2000 - available_items
-        #restocked_units = 69
         #running total of difference between 2000 items (total sales)
-        
         # sold units will be updated in sales() after first day
-        #inventory_records.append(current_day, sold_units, restocked_units, available_items)
+        
         # TESTING ROW ISSUE:
         # inventory_records.append(current_day)
         # inventory_records.append(sold_units)
         # inventory_records.append(restocked_units)
         # inventory_records.append(available_items)
         # initializing list
+
+        ## updating items available after restock (always 2000 but used this line for testing)
         available_items = available_items + restocked_units 
-        sub_list = [current_day, sold_units, restocked_units, available_items]
-        inventory_records.append(sub_list)
-        # res = []
-        # for row in inventory_records:
- 
-        #     flag = True
- 
-        #     # checking for all elements in list
-        #     for ele in sub_list:
-        #         if ele not in row:
-        #             flag = False
- 
-        #     if flag:
-        #         res.append(row)
- 
-        # # printing result
-        # print("Rows with list elements : " + str(res))
 
-       # current_day = current_day + 1
-
+    ##if stock does not need to be replenished, restocked items must be 0
     elif restock_needed == False:
         restocked_units = 0
-        # inventory_records.append(current_day)
-        # inventory_records.append(sold_units)
-        # inventory_records.append(restocked_units)
-        # inventory_records.append(available_items)
-        
-        
-        sub_list = [current_day, sold_units, restocked_units, available_items]
-        inventory_records.append(sub_list)
 
-        
+
+    ## use of sub list to update inventory records
+    sub_list = [current_day, sold_units, restocked_units, available_items]
+    inventory_records.append(sub_list)
 
     return available_items
--- sales.py
+++ sales.py
@@ -1,52 +1,35 @@
 import random
 
 def daily_sales(available_items, sold_units, inventory_records, current_day, restocked_units):
-    '''
-***********COMPLETE THIS FUNCTION***********
-This function is responsible for updating the sales for a given day.
----------------
-Function Input:
----------------
-available_items: (integer) Available Tshirts from the previous day.
-inventory_records: (List) A list of inventory records until the previous day. Each row contains (day, sales, restocked items, available items)
-current_day: (integer) Day number which you want to add as the current day. 
 
-----------------
-Function Output:
-----------------
-available_items:(integer) This function returns this integer which updates the available items at the current day.
-
-The function will also update the inventory_records (For restocking) for a  given current day. 
-
-    '''
-     ## generating sales
-   ######################################
    # CODE FROM W3 SCHOOLS:
    #import random
    #print(random.randrange(1,10))
    ########################################
+   #CODE FROM STACK OVERFLOW (NOT IMPLEMENTED HOWEVER HELPED MY CODE TESTING)
     #inventory_records.remove(current_day)
     #inventory_records.remove(available_items)
+    ########################################
+    #CODE FROM GEEKFORGEEKS
+    #(Specifically the syntax for indexing a list of lists)
+    #inventory_records[position_of_subset][1] = sold_units
+    #inventory_records[position_of_subset][3] = available_items
 
-    
+    ## generating random number on sales days, on restock days sales = 0
     import random
-    if ((current_day % 7) == 0):
+    if ((current_day % 7) == 0) or current_day == 0:
         sold_units = 0
-    elif current_day == 0:
-        sold_units = 0   
     else:
-        sold_units =  random.randrange(0,201) ### check this range
+        sold_units =  random.randrange(0,201) ### random number between 0 and 200 (inclusive)
         
-
-
+    #updating items available
     available_items = available_items - sold_units
+    #used sublist to make indexing easier
     subset = [current_day, sold_units, restocked_units, available_items]
+    #this variable was used to differentiate and make code more readable
     position_of_subset = int(current_day)
+    # updating these two variables using indexing
     inventory_records[position_of_subset][1] = sold_units
     inventory_records[position_of_subset][3] = available_items
-    #inventory_records.update(current_day, available_items)
-   ## don't need to update current day?
-   ## don't need to update inventory records as this is current not past?
-   #inventory_records.update(available_items)
-    
+     
     return available_items
